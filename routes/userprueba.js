542717c2456b52ccd3779a8d1e2e26d1e567225b
Victoria Acuna <victoria.acuna@correo.unimet.edu.ve>
2020-06-07 21:57:53 -0400

    usersprubea

--- routes/userprueba.js
+++ routes/userprueba.js
@@ -9,14 +9,16 @@ router.post('/adduser', async (req, res) => {
         const {
             firstName,
             lastName,
-            email
+            email,
+            editable
         } = req.body
 
         
         const user = await User.create({
             firstName,
             lastName,
-            email
+            email,
+            editable
         });
 
         
@@ -71,6 +73,94 @@ router.get('/', async (req, res) => {
       }
     }
   
+  });
+
+  router.get('/:id', async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+  
+      return res.status(200).json({
+        success: true,
+        data: user
+      });
+  
+    } catch (err) {
+      if (err.name === 'ValidationError') {
+        const messages = Object.values(err.errors).map(val => val.message);
+  
+        return res.status(400).json({
+          success: false,
+          error: messages
+        });
+      } else {
+        return res.status(500).json({
+          success: false,
+          error: 'Server Error ' + err
+        });
+      }
+    }
+  
+  });
+
+  router.delete('/delete/:id', async (req, res) => {
+    try {
+      // const user = await User.findById(req.params.id);
+      User.deleteOne({_id: req.params.id}, (err, user) => {
+        if(err){
+            res.json(err);
+        }
+        else {
+            res.json(user);
+        }
+    })
+  
+    } catch (err) {
+      if (err.name === 'ValidationError') {
+        const messages = Object.values(err.errors).map(val => val.message);
+  
+        return res.status(400).json({
+          success: false,
+          error: messages
+        });
+      } else {
+        return res.status(500).json({
+          success: false,
+          error: 'Server Error ' + err
+        });
+      }
+    }
+  
+  });
+
+
+  router.put('/update/:id', async (req, res) => {
+    try {
+  
+      const {
+        firstName,
+        lastName,
+        email,
+        editable,
+      } = req.body
+  
+      const newUser = await User.findOneAndUpdate({ _id: req.params.id }, {
+        firstName,
+        lastName,
+        email,
+        editable
+      }, { returnOriginal: false, useFindAndModify: false });
+  
+      return res.status(200).json({
+        success: true,
+        data: newUser
+      });
+  
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        error: 'Server Error ' + err
+      });
+    }
   });
 
   module.exports = router;
