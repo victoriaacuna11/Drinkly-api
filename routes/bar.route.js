fd0345cc60d1742389d09ea82ca1595b182960d0
Victoria Acuna <victoria.acuna@correo.unimet.edu.ve>
2020-06-09 21:14:01 -0400

    create bar

--- routes/bar.route.js
+++ routes/bar.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Bar = require('../models/Bar');
 
 
-//GET ADS
+//GET BARS
 
 router.get('/', async (req, res) => {
     try {
@@ -33,47 +33,51 @@ router.get('/', async (req, res) => {
   });
 
   
-  // ADD  BAR
+// ADD  BAR
 router.post('/add', async (req, res) => {
+  console.log('hi');
   try {
+      const sm = {
+        twitter: req.body.twitter,
+        insta: req.body.instagram,
+        email: req.body.email,
+        facebook: req.body.facebook
+        
+      }
+      console.log(sm);
+      const loc = {
+        zone: req.body.zone,
+        address: req.body.address
+      }
+      console.log(loc);
       // Destructuring de lo que manda el usuario
-      const {
-          name,
-          working_hours,
-          rating,
-          cost,
-          phone,
-          pictures,
-          menu,
-          social_media,
-          available,
-          associate,
-          location,
-          views
-      } = req.body
-
-      
-      const bar = await Bar.create({
-        name,
-        working_hours,
-        rating,
-        cost,
-        phone,
-        pictures,
-        menu,
-        social_media,
-        available,
-        associate,
-        location,
-        views
-      });
+      const bar = {
+        name: req.body.name,
+        main_image: req.body.main_image,
+        description: req.body.description,
+        working_hours: req.body.working_hours,
+        rating: req.body.rating,
+        cost: req.body.cost,
+        phone: req.body.phone,
+        pictures: req.body.pictures,
+        menu: req.body.menu,
+        social_media: sm,
+        available: req.body.available,
+        associate: req.body.associate,
+        location: loc,
+        views: req.body.views
+        
+      }
+      console.log(bar);
+      const newBar = await Bar.create(bar);
 
       
       // Send a new token to the client (frontend)
       return res.status(200).json({
+          
           success: true,
           // token,
-          bar
+          newBar
       });
 
   } catch (err) {
